e28580f854d02cc57e46b67d9c21200464a97ff5
Munkiestar <munkieryder@gmail.com>
2024-02-19 11:37:39 +0100

    add/register - bcrypt password

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,6 @@
 import express from "express";
+import bcrypt from "bcrypt";
+
 import User from "../models/User.js";
 
 const router = express.Router();
@@ -6,9 +8,23 @@ const router = express.Router();
 // REGISTER
 router.post("/register", async (req, res) => {
   try {
-    console.log("req body: ", req.body);
-    const newUser = new User(req.body);
-    //
+    const { username, email, password } = req.body;
+
+    // check if user exists
+    const existingUser = await User.findOne({
+      $or: [{ username }, { email }],
+    });
+
+    if (existingUser)
+      return res.status(400).json("Username or email already exists!");
+
+    const salt = await bcrypt.genSalt(10);
+    // encrypt the password
+    const hashedPass = await bcrypt.hashSync(password, salt);
+
+    // create new User
+    const newUser = new User({ ...req.body, password: hashedPass });
+    // push user to Mongo db
     const user = await newUser.save();
     res.status(201).json(user);
   } catch (err) {
